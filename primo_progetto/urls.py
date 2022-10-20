fa7f1d9ac95362b30f3bd5fe4ade29ee51b7c0e5
Giampiero <giampiero.foppapedretti@alessandrinimainardi.edu.it>
2022-10-20 11:46:37 +0200

    passo11

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('prima_app/', include('prima_app.urls', namespace = "prima_app")), 
     path('seconda_app/', include('seconda_app.urls', namespace = "seconda_app")),
-    path('/', index_generale, name = "index_generale"),
-   # path('/', include, namespace = "base")
+    path('', index_generale, name = "index_generale"),
+   
 ]
--- primo_progetto/views.py
+++ primo_progetto/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index_generale(request):
     return render(request, "index_generale.html")
-
-#def base(request):
- #  return render(request, "base.html")
